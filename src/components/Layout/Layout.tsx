804cad6084342d92c75a0020cbbb3d540c0f134b
Lior <levydlior@gmail.com>
2023-01-14 16:14:43 -0700

    removed fake task array

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,31 +1,7 @@
 import { useEffect, useState } from "react";
-import { priorityTypes, TaskItem } from "../TaskItem/TaskItem";
+import { TaskItem } from "../TaskItem/TaskItem";
 import { AddTaskButton, MainWrapper } from "./Layout.style";
 
-const fakeTasks = [
-  {
-    id: 1,
-    title: "Clean car",
-    dueDate: "1/13/2022",
-    completed: false,
-    //dont know if this is best practice. To look into it
-    priority: "low" as priorityTypes,
-  },
-  {
-    id: 2,
-    title: "Do Laundry",
-    dueDate: "1/14/2022",
-    completed: true,
-    priority: "medium" as priorityTypes,
-  },
-  {
-    id: 3,
-    title: "Groceries",
-    dueDate: "1/15/2022",
-    completed: false,
-    priority: "high" as priorityTypes,
-  },
-];
 
 export const Layout = () => {
 
